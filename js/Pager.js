ebc32b7729172b78c3d87bef1662abea28476b52
WRY <gundamexia1997@gmail.com>
2017-08-30 17:07:37 +0800

    Dynamic Page script and template

--- js/Pager.js
+++ js/Pager.js
@@ -1,44 +1,53 @@
 //Javascript for dynamic page arrangement
 
-var ListDiv = document.getElementById("post-preview-list");
+function renderList(posts){
 
-var posts = [
-    {
-        "href": "../posts/first_post",
-        "title": "First Post",
-        "subtitle": ":D",
-        "date": "April 2, 2017",
-    },
+    var ListDiv = document.getElementById("post-preview-list");
 
-    {
-        "href": "../posts/dell_review",
-        "title": "Two days into the red GTR and I'm lovin' it.",
-        "subtitle": "My first impressions on Dell's budget gaming laptop - Inspiron 15 (7567)",
-        "date": "April 12,2017", 
+    //DOM structure
+    posts.forEach(function(post){
+
+        var postpreviewDiv = document.createElement("div");
+        var hrefDiv = document.createElement("a");
+        var posttitleDiv = document.createElement("h2");
+        var postsubtitleDiv = document.createElement("h3");
+        var postdateDiv = document.createElement("p");
+
+        postpreviewDiv.setAttribute("class", "post-preview");
+        hrefDiv.href = post.href;
+        posttitleDiv.setAttribute("class", "post-title");
+        posttitleDiv.innerHTML = post.title;
+        postsubtitleDiv.setAttribute("class", "post-subtitle");
+        postsubtitleDiv.innerHTML = post.subtitle;
+        postdateDiv.setAttribute("class", "post-meta");
+        postdateDiv.innerHTML = post.date;
+
+        hrefDiv.appendChild(posttitleDiv);
+        hrefDiv.appendChild(postsubtitleDiv);
+        
+        postpreviewDiv.appendChild(hrefDiv);
+        postpreviewDiv.appendChild(postdateDiv);
+
+        ListDiv.appendChild(postpreviewDiv);
+    });
+}
+
+
+    function getPosts(){
+        return [
+            {
+                "href": "../posts/dell_review",
+                "title": "Two days into the red GTR and I'm lovin' it.",
+                "subtitle": "My first impressions on Dell's budget gaming laptop - Inspiron 15 (7567)",
+                "date": "April 12,2017", 
+            },
+            {
+                "href": "../posts/first_post",
+                "title": "First Post",
+                "subtitle": ":D",
+                "date": "April 2, 2017",
+            }
+        ];
     }
-];
 
-//DOM structure
-posts.forEach(function(post){
-
-    var postpreviewDiv = document.createElement("div");
-    var hrefDiv = document.createElement("a");
-    var posttitleDiv = document.createElement("h2");
-    var postsubtitleDiv = document.createElement("h3");
-    var postdateDiv = document.createElement("p");
-
-    postpreviewDiv.classList.add = "post-preview";
-    hrefDiv.href = post.href;
-    posttitleDiv.classList.add = "post-title";
-    posttitleDiv.innerHTML = post.title;
-    postsubtitleDiv.classList.add = "post-subtitle";
-    postsubtitleDiv.innerHTML = post.subtitle;
-    postdateDiv.classList.add = "post-meta";
-    postdateDiv.innerHTML = post.date;
-
-    hrefDiv.appendChild(posttitleDiv);
-    hrefDiv.appendChild(postsubtitleDiv);
-    
-    postpreviewDiv.appendChild(hrefDiv);
-    postpreviewDiv.appendChild(postdateDiv);
-})
+    renderList(getPosts());
