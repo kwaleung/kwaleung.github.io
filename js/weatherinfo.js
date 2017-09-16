ba44c7707d4d620b5d0346984f12bfd879034100
WRY <gundamexia1997@gmail.com>
2017-09-17 01:44:03 +0800

    Tidying up

--- js/weatherinfo.js
+++ js/weatherinfo.js
@@ -14,19 +14,13 @@ function getCurrentWeather(){
     var weatherinfo = new Object();
 
     locationinfo = retrieveIPlocation();
-    
-    console.log(locationinfo);
 
     _currentLat = locationinfo["latitude"];
     _currentLong = locationinfo["longitude"];
     _currentcountry = locationinfo["country_name"];
     _currentcity = locationinfo["city"];
 
-    console.log(_currentLat + " " + _currentLong + " " + _currentcountry);
-
     weatherinfo = getCurrentWeatherInfo(_currentLat, _currentLong);
-
-    console.log(weatherinfo);
 
     _weatherstatus = weatherinfo.weather[0].main;
     _currenttemp = weatherinfo.main.temp;
@@ -36,10 +30,6 @@ function getCurrentWeather(){
 
 function getCurrentWeatherInfo(Lat, Long){
     var weather = new Object();
-    // $.getJSON('https://api.openweathermap.org/data/2.5/weather?lat='+Lat+'&lon='+Long+'&units=metric&APPID=600777002eac9a316a691b9070f89457&callback=', function(data) {
-    //     console.log(data);
-    //     weather.load(data);
-    // }); 
     $.ajax(
         {
             type: 'GET',
@@ -48,7 +38,6 @@ function getCurrentWeatherInfo(Lat, Long){
             async: false,
             success: function(data)
             {
-                console.log(data);
                 weather = data;
             }
         }
@@ -58,10 +47,6 @@ function getCurrentWeatherInfo(Lat, Long){
 
 function retrieveIPlocation(){
     var location = new Object();
-    // $.getJSON('https://freegeoip.net/json/?callback=', function(data) {
-    //     console.log(data);
-    //     location.load(data);
-    // });
     $.ajax(
         {
             type: 'GET',
@@ -70,7 +55,6 @@ function retrieveIPlocation(){
             async: false,
             success: function(data)
             {
-                console.log(data);
                 location = data;
             }
         }
