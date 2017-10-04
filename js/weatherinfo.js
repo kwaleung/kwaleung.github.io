6573f9474ff2618094187c1423096cf0d351b89d
WRY <gundamexia1997@gmail.com>
2017-10-04 14:55:59 +0800

    Removed weather component

--- js/weatherinfo.js
+++ js/weatherinfo.js
@@ -1,8 +1,8 @@
 function getCurrentWeather(){
 
     //Variables
-    var _currentLat;
-    var _currentLon;
+    var _currentLat = 24.9837322;
+    var _currentLon = 121.5426982;
     var _currentcountry = "country";
     var _currentcity = "city";
     var _weatherstatus = "status";
@@ -13,17 +13,18 @@ function getCurrentWeather(){
     var locationinfo = new Object();
     var weatherinfo = new Object();
 
-    locationinfo = retrieveIPlocation();
+    locationinfo = retrievelocationkey(_currentLat, _currentLon);
 
-    _currentLat = locationinfo["latitude"];
-    _currentLong = locationinfo["longitude"];
-    _currentcountry = locationinfo["country_name"];
-    _currentcity = locationinfo["city"];
+    //_currentLat = locationinfo["latitude"];
+    //_currentLong = locationinfo["longitude"];
+    var locationkey = locationinfo[0].Key;
+    _currentcountry = locationinfo[0].Country.LocalizedName;
+    _currentcity = locationinfo[0].LocalizedName;
 
-    weatherinfo = getCurrentWeatherInfo(_currentLat, _currentLong);
+    weatherinfo = getCurrentWeatherInfo(locationkey);
 
-    _weatherstatus = weatherinfo.weather[0].description;
-    _currenttemp = weatherinfo.main.temp;
+    _weatherstatus = weatherinfo[0].WeatherText;
+    _currenttemp = weatherinfo[0].Temperature.Metric.Value;
 
     var displaystring;
 
@@ -39,12 +40,12 @@ function getCurrentWeather(){
     infoDiv.innerHTML = displaystring;
 }
 
-function getCurrentWeatherInfo(Lat, Long){
+function getCurrentWeatherInfo(LocKey){
     var weather = new Object();
     $.ajax(
         {
             type: 'GET',
-            url: 'https://api.openweathermap.org/data/2.5/weather?lat='+Lat+'&lon='+Long+'&units=metric&lang=zh_cn&APPID=600777002eac9a316a691b9070f89457&callback=',
+            url: `https://apidev.accuweather.com/currentconditions/v1/${LocKey}.json?language=zh-cn&apikey=c93f47e94a7243e3a3eb028fdbb6fbc0`,
             dataType: 'json',
             async: false,
             success: function(data)
@@ -56,12 +57,12 @@ function getCurrentWeatherInfo(Lat, Long){
     return weather;
 }
 
-function retrieveIPlocation(){
+function retrievelocationkey(lat, long){    
     var location = new Object();
     $.ajax(
         {
             type: 'GET',
-            url: 'https://freegeoip.net/json/?callback=',
+            url: `https://apidev.accuweather.com/locations/v1/geoposition/search.json?q=${lat},${long}&language=zh-cn&apikey=c93f47e94a7243e3a3eb028fdbb6fbc0`,
             dataType: 'json',
             async: false,
             success: function(data)
